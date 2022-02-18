552f55b4c6a33f02c3c90d3df771092213b08841
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-18 17:26:19 +0600

    Calculate Button Data Error handle

--- js/app.js
+++ js/app.js
@@ -1,49 +1,53 @@
 
-
 function convertToFlt(expenses) {
     const getAmount = document.getElementById(expenses);
-    // console.log(getAmount);
     let amount = parseInt(getAmount.value);
-    console.log(amount);
-    // if (isNaN(amount)) {
-    //     alert2 = alert('Hello world!');
-    // }
-    // else if (amount < 0) {
-    //     // const alert2 = alert('Hello world!');
-    //     return "6";
-    // }
-
-    if (amount < 0) {
-        alert('Hrld!');
-    }
     return amount;
 }
 function totalExpenses() {
     const allExpenses = convertToFlt('food-ex-input') + convertToFlt('rent-ex-input') + convertToFlt('clothes-ex-input');
     return allExpenses;
 }
+
 function setValue(fieldId, setFinalValue) {
     const getTotalExpenses = document.getElementById(fieldId);
     getTotalExpenses.innerText = setFinalValue;
 }
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
+    //Get Input Value
     const getIncome = convertToFlt('income-input');
+    const getFood = convertToFlt('food-ex-input');
+    const getRent = convertToFlt('rent-ex-input');
+    const getClothes = convertToFlt('clothes-ex-input');
 
-    //set total expenses
-    setValue('total-expenses', totalExpenses());
-
-    // final balance
-    const balance = getIncome - totalExpenses();
-
-    //set final Balance after expenses
-    setValue('balance', balance);
+    //Error Handle
+    if (isNaN(getIncome) || getIncome < 0) {
+        alert("Please give valid income first!!");
+    }
+    else {
+        if (isNaN(getFood) || getFood < 0) {
+            alert("Please give valid Food expenses first!!")
+        }
+        else if (isNaN(getRent) || getRent < 0) {
+            alert("Please give valid Rent expenses first!!");
+        }
+        else if (isNaN(getClothes) || getClothes < 0) {
+            alert("Please give valid Clothes expenses first!!")
+        }
+        else {
+            //set total expenses
+            setValue('total-expenses', totalExpenses());
+            // final balance
+            const balance = getIncome - totalExpenses();
+            //set final Balance after expenses
+            setValue('balance', balance);
+        }
+    }
 });
 
 document.getElementById('save-btn').addEventListener('click', function () {
-
     const getSavePercent = convertToFlt('save-input');
-    // console.log(getSavePercent);
     if (getSavePercent < 0) {
         alert('Hrld!');
     }
